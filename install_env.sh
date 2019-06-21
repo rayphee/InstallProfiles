993fe80b4eed77d93d21644801ed576495169b73
Rafi Mueen <rafi@Rafis-MacBook-Pro.local>
2019-06-21 14:11:07 -0400

    Fixed the oh-my-zsh directory thing; probably still not using it

--- install_env.sh
+++ install_env.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+echo "Installing Rafi's environment profiles"
+echo "Note: git, curl, and development tools are expected to be already installed"
+echo "Currently supports: nothing"
+
+if [ ! -x "$(command -v brew)" ]; then
+	echo "Homebrew not found. Installing..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 brew update
-brew install tmux
-mv files ~/
+
+if [ ! -x "$(command -v tmux)" ]; then
+	echo "TMux not found. Installing..."
+	brew install tmux
+fi
+if [ ! -x "$(command -v zsh)" ]; then
+	echo "ZShell not found. Installing..."
+	brew install zsh
+fi
+if [ ! -x "$(command -v vim)" ]; then
+	echo "VIm not found. Installing..."
+	brew install vim
+fi
+
+
+
+# mv files ~/
