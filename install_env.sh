2a4814cdc046014a9fe207d2da9028eafae8b86f
Rafi Mueen <r.mueen@gmail.com>
2020-04-18 22:56:05 -0400

    Added initial support for Linux (debian based, tested on Ubuntu 20.04 LTS)

--- install_env.sh
+++ install_env.sh
@@ -1,68 +1,109 @@
-#!/bin/sh
+#!/bin/bash
 
 declare -a configuration_files=("tmux.conf" "vimrc" "zshrc")
 declare -a distribution_folders=("tmux" "vim")
+declare -a missing_programs=()
 
 echo "Installing Rafi's environment profiles"
 echo "Note: git, curl, and development tools are expected to be already installed"
-echo "The configuration profiles are written to comply with the latest versions of brew, tmux, zsh, and vim as of 6/21/19"
+echo "The configuration profiles are written to comply with the latest versions of brew, tmux, zsh, and vim as of 4/18/19"
 echo "Currently supports: macOS Mojave+"
-
-if [ ! -x "$(command -v brew)" ]; then
-	echo "Homebrew not found. Installing..."
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew update
+echo "	macOS (Tested on Mojave)"
+echo "	Linux (Tested on Ubuntu 20.04 LTS, Debian based only)"
 
 if [ ! -x "$(command -v tmux)" ]; then
-	echo "TMux not found. Installing..."
-	brew install tmux
+	missing_programs+=("tmux")
 fi
 if [ ! -x "$(command -v zsh)" ]; then
-	echo "ZShell not found. Installing..."
-	brew install zsh
+	missing_programs+=("zsh")
 fi
 if [ ! -x "$(command -v vim)" ]; then
-	echo "VIm not found. Installing..."
-	brew install vim
+	missing_programs+=("vim")
+fi
+if [ ! -x "$(command -v pip3)" ]; then
+	missing_programs+=("python3-pip")
 fi
 
+run_mac_setup () {
+	if [ ! -x "$(command -v brew)" ]; then
+		echo "Homebrew not found. Installing..."
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+	
+	brew update
+	
+	for i in "${missing_programs[@]}"
+	do
+		brew install $i
+	done
 
-echo "Copying distribution folders..."
-for i in "${distribution_folders[@]}"
-do
-	tar -xzf $i.tgz -C ~
-	mv ~/$i ~/.$i
-done
+	echo "Installing fonts..."
+	cp fonts/operator_mono/* ~/Library/Fonts
+	
 
-echo "Installing TPM..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	run_generic_setup
+}
 
-echo "Installing Oh My Zsh..."
-curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-sh install.sh --unattended
-cp agnoster-simple.zsh-theme ~/.oh-my-zsh/themes
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-rm -rf install.sh # GDI Oh My Zsh why're you leaving your trash here?
+run_ubuntu_setup () {
+	if [ ! -z "$missing_programs" ]; then
+		sudo apt install ${missing_programs[@]} -y
+	fi
 
-echo "Installing Vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-pip install --user powerline-status
+	echo "Installing fonts..."
+	if [ ! -d "~/.fonts" ]; then
+		mkdir ~/.fonts
+		cp -r fonts/operator_mono/ ~/.fonts/
+	fi
 
-echo "Installing fonts..."
-cp fonts/operator_mono/* ~/Library/Fonts
+	run_generic_setup
+}
 
-echo "Copying configuration files..."
-for i in "${configuration_files[@]}"
-do
-	cp $i ~/.$i
-done
+run_generic_setup (){
+	echo "Copying distribution folders..."
+	for i in "${distribution_folders[@]}"
+	do
+		tar -xzf $i.tgz -C ~
+		mv ~/$i ~/.$i
+	done
+	
+	echo "Installing TPM..."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	
+	echo "Installing Oh My Zsh..."
+	curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+	sh install.sh --unattended
+	cp agnoster-simple.zsh-theme ~/.oh-my-zsh/themes
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	rm -rf install.sh # GDI Oh My Zsh why're you leaving your trash here?
+	
+	echo "Installing Vundle..."
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	pip3 install --user powerline-status
+	
+	echo "Copying configuration files..."
+	for i in "${configuration_files[@]}"
+	do
+		cp $i ~/.$i
+	done
+	
+	echo "Applying profile changes..."
+	tmux source ~/.tmux.conf
+	
+	chsh -s $(which zsh)
+	
+	exit 0
+}
 
-echo "Applying profile changes..."
-tmux source ~/.tmux.conf
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	echo "Linux-based detected"
+	run_ubuntu_setup
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "Darwin-based OS detected"
+	run_mac_setup
+else
+        echo "Unsupported OS detected"
+	echo "Aborting environment setup"
+	exit -1
+fi
 
-chsh -s $(which zsh)
-
-exit 0
--- zshrc
+++ zshrc
@@ -8,7 +8,7 @@ export DEFAULT_USER=rafi
 prompt_context(){}
 
 # Path to your oh-my-zsh installation.
-export ZSH=/Users/rafi/.oh-my-zsh
+export ZSH=~/.oh-my-zsh
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
